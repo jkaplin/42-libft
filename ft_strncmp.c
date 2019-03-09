4ac43ab13bcd2a4c6e959bab9c3e2a18efa25625
Joseph Kaplin <jkaplin@e1z2r7p17.42.us.org>
2019-03-09 09:29:04 -0800

    update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jkaplin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 20:04:24 by jkaplin           #+#    #+#             */
-/*   Updated: 2019/03/04 20:34:33 by jkaplin          ###   ########.fr       */
+/*   Updated: 2019/03/09 09:28:01 by jkaplin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	if (i == n)
 		return (0);
